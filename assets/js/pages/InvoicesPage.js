ecb0012163f09f70b21df2fd09a11738cb362a63
Axel Redois <axelredois25@gmail.com>
2020-02-21 10:40:46 +0100

    finishing invoicesPage, refactoring and add comments

--- assets/js/pages/InvoicesPage.js
+++ assets/js/pages/InvoicesPage.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import Pagination from '../components/Pagination';
-import axios from 'axios';
 import moment from 'moment';
+import invoicesAPI from '../services/invoicesAPI';
 
 const STATUS_CLASSES = {
   PAID: "success",
@@ -15,23 +15,25 @@ const STATUS_LABELS = {
   CANCELLED: "Annulée"
 };
 
-const InvoicesPage = props => {
+const InvoicesPage = () => {
 
   const [invoices, setInvoices] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [search, setSearch] = useState("");
 
+  const itemsPerPage = 10;
+
+    // Permet d'aller récuperer les invoices
   const fetchInvoices = async () => {
     try {
-      const data = await axios
-      .get("http://127.0.0.1:8000/api/invoices")
-      .then(response => response.data["hydra:member"]);
+      const data = await invoicesAPI.findAll()
       setInvoices(data);
     } catch(error) {
       console.log(error.response);
     }
   };
 
+  // Au chargement du composant, on va chercher les invoices
   useEffect(() => {
     fetchInvoices();
   }, []);
@@ -43,11 +45,32 @@ const InvoicesPage = props => {
   const handleSearch = event => {
     setSearch(event.target.value);
     setCurrentPage(1);
-  }
+  };
 
-  const itemsPerPage = 10;
+  // Gestion de la suppression d'un invoice
+  const handleDelete = async id => {
+    const originalInvoices = [...invoices];
 
+    setInvoices(invoices.filter(customer => customer.id !== id));
+
+    try {
+      await invoicesAPI.delete(id)
+    } catch(error) {
+      setInvoices(originalInvoices);
+    }
+  };
+
+  // Gestion de date
   const formatDate = (str) => moment(str).format('DD/MM/YYYY');
+
+  // Filtrage des invoices en fonction de la recherche
+  const filteredInvoices = 
+  invoices.filter(invoice => 
+    invoice.customer.lastName.toLowerCase().includes(search.toLowerCase()) || 
+    invoice.customer.firstName.toLowerCase().includes(search.toLowerCase()) ||
+    invoice.amount.toString().includes(search.toLowerCase()) ||
+    STATUS_LABELS[invoice.status].toLowerCase().includes(search.toLowerCase())
+    );
 
   // Pagination des données
   const paginatedInvoices = Pagination.getData(filteredInvoices, currentPage, itemsPerPage);
@@ -55,6 +78,15 @@ const InvoicesPage = props => {
   return (
     <Fragment>
       <h1>Liste des factures</h1>
+
+      <div className="form-group">
+        <input 
+        className="form-control"
+        placeholder="Rechercher..."
+        onChange={handleSearch}
+        value={search}
+        />
+      </div>
 
       <table className="table table-hover">
         <thead>
@@ -81,7 +113,12 @@ const InvoicesPage = props => {
               <td className="text-center">{invoice.amount.toLocaleString("fr-FR")} €</td>
               <td>
                 <button className="btn btn-sm btn-primary mr-1">Modifier</button>
-                <button className="btn btn-sm btn-danger">Supprimer</button> 
+                <button
+                className="btn btn-sm btn-danger"
+                onClick={() => handleDelete(invoice.id)}
+                >
+                  Supprimer
+                </button> 
               </td>
             </tr>
           )}
@@ -92,7 +129,7 @@ const InvoicesPage = props => {
       currentPage={currentPage}
       itemsPerPage={itemsPerPage}
       onChangePage={handleChangePage}
-      length={invoices.length}
+      length={filteredInvoices.length}
       />
     </Fragment>
   );
