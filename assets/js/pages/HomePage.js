10d4d556c858a2bd3e802db3d1ae1b02e685b205
Axel Redois <axelredois25@gmail.com>
2020-02-23 14:32:55 +0100

    change content home page and add readme

--- assets/js/pages/HomePage.js
+++ assets/js/pages/HomePage.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
     <div className="jumbotron">
-      <h1 className="display-3">Hello, world!</h1>
-      <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <h1 className="display-3">Bienvenue sur API-CRM</h1>
+      <p className="lead">API-CRM vous permet de gérer vos clients et leurs factures très simplement sur une interface minimaliste, sécurisée et performante.</p>
       <hr className="my-4"/>
-      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <p>Pour débuter, commencez par vous inscrire !</p>
       <p className="lead">
-        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        <Link className="btn btn-info btn-lg" to="/register" role="button">Je m'inscris !</Link>
       </p>
     </div>
   )
